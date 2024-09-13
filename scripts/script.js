a5caad3a74397c330c0359bedfb26663510aa919
misama17 <milamassaro17@gmail.com>
2024-09-13 08:49:00 +0200

    Hamburgermenu basis

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,20 @@
 // JavaScript Document
 console.log("hi");
 
+// CONSTANTEN
+const openButton = document.querySelector("header > button");
+const navigation = document.querySelector("nav");
+const closeButton = document.querySelector("nav button");
+
+// ---------- MENU ----------
+function openMenu() {
+    navigation.classList.toggle("toonMenu");
+    console.log("change button?");
+}
+
+function closeMenu() {
+    navigation.classList.remove("toonMenu");
+}
+
+openButton.onclick = openMenu;
+closeButton.onclick = closeMenu;
